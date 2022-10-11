fa5352b8c514f500125cdf5b3b501fd2cf425530
jake <palatable7@naver.com>
2022-10-11 09:42:27 +0900

    Initial Commit

---
+++ ExShapes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ExShapes
+//
+//  Created by 김종권 on 2022/10/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ExShapes/ExShapesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExShapesApp.swift
+//  ExShapes
+//
+//  Created by 김종권 on 2022/10/11.
+//
+
+import SwiftUI
+
+@main
+struct ExShapesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
